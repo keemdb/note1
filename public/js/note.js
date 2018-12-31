6a7e1096f29c5c44bed5be1b71cfd814fa039cee
keemdb <keemdb@gmail.com>
2018-12-31 10:50:03 +0900

    코미토

---
+++ public/js/note.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBBcOP_RD50NoCKt0BDlM4Xu-K2CQ7x8zs",
+    authDomain: "keemdb-note.firebaseapp.com",
+    databaseURL: "https://keemdb-note.firebaseio.com",
+    projectId: "keemdb-note",
+    storageBucket: "keemdb-note.appspot.com",
+    messagingSenderId: "440886800502"
+  };
+  firebase.initializeApp(config);
